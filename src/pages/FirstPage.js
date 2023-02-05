af7a68758003ef7d9338c575207635a5a5abb1f6
LNR14 <lnrabc@gmail.com>
2023-02-05 15:27:14 -0600

    Started first challenge

---
+++ src/pages/FirstPage.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function FirstPage() {
+  return (
+    <div className="App">
+      <h3>React Facts</h3>
+      <h4>React course - Project 1</h4>
+      <hr/>
+    </div>
+  );
+}
