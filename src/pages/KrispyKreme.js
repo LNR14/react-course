93fc1c736ae747001266ddc9e0b12653f7ef5207
LNR14 <lnrabc@gmail.com>
2023-03-30 13:13:18 -0500

    added image

--- src/pages/KrispyKreme.js
+++ src/pages/KrispyKreme.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
 import Navbar from "../components/KrispyKreme/Navabar1"
-
+import logo from "../images/elena-koycheva-PFzy4N0_R3M-unsplash.jpg"
+import "./CSS/KrispyKreme/krispyKreme.css"
 
 export default function KrispyKreme() {
 
   return (
     <div className="App">
       <Navbar/>
+      <br/>
+      <Container>
+        <Row >
+          <Col  >
+          <img src={logo} className="image1" alt="images" >
+          </img>
+          </Col>
+        </Row>
+        
+      </Container>
       
     </div>
   );
